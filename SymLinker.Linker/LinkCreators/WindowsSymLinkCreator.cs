2c7fd3862eab681d04ac73f5ae2116bc26789ea1
Samuel Huff <contact@samueljhuff.com>
2017-06-19 20:07:28 -0700

    Initial commit

---
+++ SymLinker.Linker/LinkCreators/WindowsSymLinkCreator.cs
@@ -0,0 +1,37 @@
+﻿using SymLinker.Linker;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace SymLinker.Linker.LinkCreators
+{
+    internal class WindowsSymLinkCreator : ISymLinkCreator
+    {
+        [DllImport("kernel32.dll")]
+        static extern bool CreateSymbolicLink(string lpSymlinkFileName, string lpTargetFileName, SymbolicLink dwFlags);
+
+        public bool CreateSymLink(string linkPath, string targetPath, bool file)
+        {
+            var linkName = Path.GetFileName(targetPath);
+            linkPath = Path.Combine(linkPath, linkName);
+            bool success = false;
+            try
+            {
+                var symbolicLinkType = file ? SymbolicLink.File : SymbolicLink.Directory;
+                success = CreateSymbolicLink(linkPath, targetPath, symbolicLinkType);
+            }
+            catch(Exception ex)
+            {
+                // TODO: Handle exception
+            }
+            return success;
+        }
+
+        private enum SymbolicLink
+        {
+            File = 0,
+            Directory = 1
+        }
+
+    }
+}
--- SymLinker/Program.cs
+++ SymLinker/Program.cs
@@ -1,12 +1,67 @@
-﻿using System;
+﻿using SymLinker.Linker;
+using System;
+using System.IO;
 
 namespace SymLinker
 {
     class Program
     {
+        static ConsoleColor bgc;
+        static ConsoleColor fgc;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Clear and setup for test
+            Directory.Delete("C:\\Test\\Sub", true);
+            Directory.CreateDirectory("C:\\Test\\Sub");
+            File.WriteAllText("C:\\Test\\testdoc.txt", "This is a test file", System.Text.Encoding.ASCII);
+
+            // Create and setup the symlinker
+            var linker = new SymLinker.Linker.Linker();
+            linker.OnError += WriteError;
+            linker.OnWarn += WriteWarn;
+            linker.OnInfo += WriteInfo;
+
+            // Create a link!
+            linker.CreateLink(@"C:\Test\testdoc.txt", @"C:\Test\Sub");
+        }
+
+        static void WriteError(string msg)
+        {
+            SaveConsoleColors();
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(msg);
+            RestoreConsoleColors();
+        }
+
+        static void WriteInfo(string msg)
+        {
+            SaveConsoleColors();
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(msg);
+            RestoreConsoleColors();
+        }
+
+        static void WriteWarn(string msg)
+        {
+            SaveConsoleColors();
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(msg);
+            RestoreConsoleColors();
+        }
+
+        static void SaveConsoleColors()
+        {
+            fgc = Console.ForegroundColor;
+            bgc = Console.BackgroundColor;
+        }
+
+        static void RestoreConsoleColors()
+        {
+            Console.ForegroundColor = fgc;
+            Console.BackgroundColor = bgc;
         }
     }
 }
